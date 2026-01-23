22a0c1a329a71fdfecf74273ac59cbb930fb2d2b
Stephen Sparling <sparlingcreations@gmail.com>
2026-01-23 13:26:56 -0500

    add react aria, install button, render on web app

---
+++ packages/ui/src/Button/Button.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  composeRenderProps,
+  Button as RACButton,
+  type ButtonProps as RACButtonProps,
+} from "react-aria-components";
+import { ProgressCircle } from "../ProgressCircle/ProgressCircle";
+import "./button.css";
+
+interface ButtonProps extends RACButtonProps {
+  /**
+   * The visual style of the button (Vanilla CSS implementation specific).
+   * @default 'primary'
+   */
+  variant?: "primary" | "secondary" | "quiet";
+}
+
+export function Button(props: ButtonProps) {
+  return (
+    <RACButton
+      {...props}
+      className="react-aria-Button button-base"
+      data-variant={props.variant || "primary"}
+    >
+      {composeRenderProps(props.children, (children, { isPending }) => (
+        <>
+          {!isPending && children}
+          {isPending && (
+            <ProgressCircle aria-label="Saving..." isIndeterminate />
+          )}
+        </>
+      ))}
+    </RACButton>
+  );
+}
