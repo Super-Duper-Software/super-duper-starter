da6395b4eaa210cbf5dba60a63862724bdcdc06d
Stephen Sparling <sparlingcreations@gmail.com>
2026-02-01 20:51:51 -0500

    fix type module issues with local invocation of jobs

--- packages/jobs/src/cli.ts
+++ packages/jobs/src/cli.ts
@@ -1,6 +1,9 @@
 import fs from "node:fs";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import cac from "cac";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const cli = cac();
 
